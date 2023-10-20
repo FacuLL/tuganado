92f6ab2974c36f8664a29666b7d3e242a332e759
Facundo López Llopis <faculopllop@gmail.com>
2023-10-20 16:38:53 -0300

    chore: general

--- frontend/src/app/historical/historical.page.ts
+++ frontend/src/app/historical/historical.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CowService } from '../services/cow.service';
+import { RecordService } from '../services/record.service';
+import { FileTransfer, FileTransferObject } from '@awesome-cordova-plugins/file-transfer/ngx';
 
 @Component({
   selector: 'app-historical',
@@ -9,16 +11,34 @@ import { CowService } from '../services/cow.service';
 export class HistoricalPage implements OnInit {
 
   selectedCow?: string;
+  records?: any[];
 
   cows?: any[];
 
-  constructor(private cowService: CowService) { }
+  fileTransfer: FileTransferObject = this.transfer.create();
+
+  constructor(private cowService: CowService, private recordService: RecordService, private transfer: FileTransfer) {}
 
   ngOnInit() {
     this.getCows();
   }
 
-  async getCows() {
+  selectChange(event: any) {
+    this.getRecords(event.target.value);
+  }
+
+  getRecords(caravana: string) {
+    this.cowService.getCow(caravana).subscribe({
+      next: (res) => {
+        this.records = res.records;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+  getCows() {
     this.cowService.getCows().subscribe({
       next: (res) => {
         this.cows = res;
@@ -29,6 +49,24 @@ export class HistoricalPage implements OnInit {
     })
   }
 
-  downloadData() {}
+  stringifyDate(date: string) {
+    let d = new Date(date);
+    let day = d.getDate()+1;
+    let month = d.getMonth()+1;
+    let year = d.getFullYear();
+    return `${day}/${month}/${year}`
+  }
+
+
+  loadingData: boolean = false;
+  downloadData() {
+    this.loadingData = true;
+    this.fileTransfer.download(this.recordService.downloadDataURL(), 'file.xslx').then((entry) => {
+      console.log('download complete: ' + entry.toURL());
+      this.loadingData = false;
+    }, (error) => {
+      console.log(error);
+    });
+  }
 
 }
